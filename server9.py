c7248f81b04fcb997265d800c68969ee2fefb1a3
c00dex2022 <98943346+c00dex2022@users.noreply.github.com>
2023-02-21 15:29:35 +0300

    Create server9.py

---
+++ server9.py
@@ -0,0 +1,40 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import csv
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # extract the requested URL
+        url = self.path.split("?")[0]
+        if "i=" in self.path:
+            i_param = self.path.split("i=")[1].split("&")[0]
+            url += "?i=" + i_param
+        
+        # log the URL to the console
+        print("Requested URL:", url)
+
+        # remove "/?i=" from the URL and write it to a CSV file
+        with open("output1.csv", "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([url.replace("/?i=", "")])
+
+        # send an empty response with a 200 status code
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"")
+
+if __name__ == "__main__":
+    # specify the server address and port
+    server_address = ("", 8080)
+
+    # create the HTTP server
+    httpd = HTTPServer(server_address, RequestHandler)
+    print("Server running at http://localhost:8080")
+
+    try:
+        # serve requests indefinitely
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        # gracefully shut down the server on Ctrl-C
+        httpd.server_close()
+        print("Server stopped")
